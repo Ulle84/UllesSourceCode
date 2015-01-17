d9a6aa6ca141a19bed71a7e3b5ac616af312eb60
Ulrich Belitz <u.belitz@icloud.com>
2015-01-17 16:57:47 +0100

    under construction

---
+++ TuKi/ArticleList/PHP/uniqueIdListCreator.php
@@ -0,0 +1,30 @@
+<?php include('authentication.php'); ?>
+<!DOCTYPE html>
+<meta charset="utf-8">
+<html>
+<head>
+    <title>Unique ID Creator</title>
+    <link rel="stylesheet" type="text/css" href="../CSS/global.css" media="all"/>
+</head>
+<body>
+
+<?php
+
+include('navigation.php');
+
+require_once 'UniqueIdList.php';
+require_once 'Settings.php';
+
+$settings = new Settings();
+
+
+
+$uniqueIdList = new UniqueIdList();
+$uniqueIdList->createNewList($settings->minSellerNumber, $settings->maxSellerNumber);
+
+$uniqueIdList->writeToFile("../Data/uniqueIds.txt");
+$uniqueIdList->printList();
+
+?>
+</body>
+</html>
