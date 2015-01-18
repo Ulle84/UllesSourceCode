ff8deb15ec2febce88d1c3a3d82923df6b4e3b92
Ulrich Belitz <u.belitz@icloud.com>
2015-01-18 07:14:50 +0100

    + Alle Artikellisten der Verkäufer als Liste

---
+++ TuKi/ArticleList/PHP/articleListViewAllSellers.php
@@ -0,0 +1,34 @@
+<?php include('authentication.php'); ?>
+<!DOCTYPE html>
+<meta charset="utf-8">
+<html>
+<head>
+    <title>TuKi - Alle Verkäuferlisten</title>
+    <link rel="stylesheet" type="text/css" href="../CSS/global.css" media="all"/>
+</head>
+
+<body>
+<?php
+
+include('navigation.php');
+
+require_once 'UniqueIdList.php';
+require_once 'ArticleList.php';
+require_once 'Settings.php';
+
+$uniqueIdList = new UniqueIdList();
+$uniqueIdList->readFromFile("../Data/uniqueIds.txt");
+
+$settings = new Settings();
+
+for ($sellerNumber = $settings->minSellerNumber; $sellerNumber <= $settings->maxSellerNumber; $sellerNumber++) {
+    $id = $uniqueIdList->getId($sellerNumber);
+    $articleList = new ArticleList($sellerNumber, $id, $settings->minArticleNumber, $settings->maxArticleNumber);
+
+    echo '<a target="_blank" href="articleListView.php?seller=' . $sellerNumber . '&id=' . $id . '&mode=edit">Liste für Verkäufer ' . $sellerNumber . '</a><br />';
+}
+
+?>
+
+</body>
+</html>
--- TuKi/ArticleList/PHP/index.php
+++ TuKi/ArticleList/PHP/index.php
@@ -9,7 +9,8 @@
 <body>
 <?php include('navigation.php'); ?>
 <a href="settingsView.php">Einstellungen</a><br />
-<a href="articleListPrintAll.php">Alle Artikellisten drucken</a><br />
+<a href="articleListViewAllSellers.php">Artikellisten aller Verkäufer</a><br />
+<a href="articleListPrintAll.php" target="_blank">Alle Artikellisten drucken</a><br />
 <a href="uniqueIdListCreator.php">Neue Zugangscodes für Verkäufer erstellen (ACHTUNG! Diese Funktion macht die alten Zugangscodes ungültig!)</a><br />
 <a href="uniqueIdListView.php">Zugangscodes für Verkäufer ansehen</a><br />
 <a href="notifySellers.php">Zugangscodes an Verkäufer senden</a><br />
