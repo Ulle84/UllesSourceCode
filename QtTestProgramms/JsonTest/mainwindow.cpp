e4270a1700783e86805d4e2b175d36a36231de35
Ulrich Belitz <u.belitz@icloud.com>
2017-03-02 21:22:59 +0100

    * update JsonTest

--- QtTestProgramms/JsonTest/mainwindow.cpp
+++ QtTestProgramms/JsonTest/mainwindow.cpp
@@ -8,44 +8,77 @@
 #include <QJsonValue>
 #include <QFile>
 #include <QDebug>
+#include <QSettings>
 
 MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
+  QMainWindow(parent),
+  ui(new Ui::MainWindow)
 {
-    ui->setupUi(this);
+  ui->setupUi(this);
+  m_settings = new QSettings("Ulle", "JsonTest", this);
 
-    QFile saveFile("/Users/Ulle/temp/test.txt");
-
-    if (!saveFile.open(QIODevice::WriteOnly))
-    {
-        qWarning("Couldn't open save file.");
-    }
-
-    QJsonObject jsonObject;
-
-    QJsonArray jsonArray;
-    for (int i = 0; i < 3; i++)
-    {
-        jsonArray.append(i+0.5);
-        /*QJsonObject jsonObject;
-        jsonObject["intTest"] = i;
-        jsonArray.append(jsonObject);*/
-    }
-
-    jsonObject["arrayTest"] = jsonArray;
-
-    for (int i = 0; i < 3; i++)
-    {
-        qDebug() << jsonObject["arrayTest"].toArray()[i].toDouble();
-    }
-
-
-    QJsonDocument jsonDocument(jsonObject);
-    saveFile.write(jsonDocument.toJson());
+  if (m_settings->contains("geometry"))
+  {
+    setGeometry(m_settings->value("geometry").toRect());
+  }
 }
 
 MainWindow::~MainWindow()
 {
-    delete ui;
+  m_settings->setValue("geometry", geometry());
+  delete ui;
+}
+
+void MainWindow::writeTestFile(const QString &path)
+{
+  QFile file(path);
+
+  if (!file.open(QIODevice::WriteOnly))
+  {
+    qWarning("Couldn't open save file.");
+  }
+
+
+  file.write(createTestObjectString());
+  file.close();
+}
+
+QByteArray MainWindow::createTestObjectString()
+{
+  QJsonObject jsonObject;
+
+  QJsonArray jsonArray;
+  for (int i = 0; i < 3; i++)
+  {
+    jsonArray.append(i+0.5);
+  }
+
+  QJsonObject jsonSubObject;
+  jsonSubObject["int"] = 100;
+  jsonSubObject["double"] = 9.983484;
+  jsonSubObject["bool"] = true;
+  jsonSubObject["string"] = QString("helloWorld");
+
+  jsonObject["arrayTest"] = jsonArray;
+  jsonObject["jsonSubObject"] = jsonSubObject;
+
+  for (int i = 0; i < 3; i++)
+  {
+    qDebug() << jsonObject["arrayTest"].toArray()[i].toDouble();
+  }
+
+
+  QJsonDocument jsonDocument(jsonObject);
+  return jsonDocument.toJson();
+}
+
+void MainWindow::on_pushButtonWriteTestFile_clicked()
+{
+  writeTestFile("/Users/Ulle/temp/test.txt");
+}
+
+void MainWindow::on_pushButtonWriteTestOutput_clicked()
+{
+  ui->plainTextEditJson->clear();
+  ui->plainTextEditJson->appendPlainText(createTestObjectString());
 }
--- QtTestProgramms/JsonTest/mainwindow.h
+++ QtTestProgramms/JsonTest/mainwindow.h
@@ -3,8 +3,11 @@
 
 #include <QMainWindow>
 
-namespace Ui {
-class MainWindow;
+class QSettings;
+
+namespace Ui
+{
+  class MainWindow;
 }
 
 class MainWindow : public QMainWindow
@@ -15,8 +18,18 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+  void writeTestFile(const QString& path);
+
+  QByteArray createTestObjectString();
+
+private slots:
+  void on_pushButtonWriteTestFile_clicked();
+
+  void on_pushButtonWriteTestOutput_clicked();
+
 private:
     Ui::MainWindow *ui;
+    QSettings* m_settings;
 };
 
 #endif // MAINWINDOW_H
